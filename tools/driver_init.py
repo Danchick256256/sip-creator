5c38bbf669d820881b96fba5f0092a92531fb84e
Danchick256 <danilfpronkin280504@gmail.com>
2021-09-17 10:12:34 +0300

    webdrivers add

--- tools/driver_init.py
+++ tools/driver_init.py
@@ -5,5 +5,5 @@ from selenium.webdriver.chrome.options import Options
 def init_driver():
     options = Options()
     options.add_argument('--headless')  # delete options=options for open browser when program start
-    driver = webdriver.Chrome(options=options, executable_path="C:\webdrivers\chromedriver.exe")
+    driver = webdriver.Chrome(options=options, executable_path=".\webdrivers\chromedriver.exe")
     return driver
